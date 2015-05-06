d08164f6e5bc964d0fa9ef2670c8e0a4146897a4
Yuri Barbosa Monteiro <yurimonteiro92@hotmail.com>
2015-05-06 17:03:28 -0300

    PessoaPOJO feita

--- Trabalho/src/Model/POJO/PessoaPOJO.java
+++ Trabalho/src/Model/POJO/PessoaPOJO.java
@@ -1,5 +1,23 @@
 package Model.POJO;
 
 public class PessoaPOJO {
+    private String nome;
+    private String cpf;
+
+    public String getNome() {
+        return nome;
+    }
+
+    public String getCpf() {
+        return cpf;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
+    }
     
 }
