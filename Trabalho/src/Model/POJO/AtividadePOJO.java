2d17160cb09bcac00733e0388d017d593da09548
wennya <wennya@wennya-Inspiron-N4050>
2015-05-06 15:11:39 -0300

    Criação de gets sets e construtores da classe

--- Trabalho/src/Model/POJO/AtividadePOJO.java
+++ Trabalho/src/Model/POJO/AtividadePOJO.java
@@ -1,5 +1,46 @@
 package POJO;
 
 public class AtividadePOJO {
+    private String nome;//verificar com equals se repete
+    private String tipo;
+    private String data;
+    private Integer valor;
+
+    public AtividadePOJO() {
+    }
+
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getTipo() {
+        return tipo;
+    }
+
+    public void setTipo(String tipo) {
+        this.tipo = tipo;
+    }
+
+    public String getData() {
+        return data;
+    }
+
+    public void setData(String data) {
+        this.data = data;
+    }
+
+    public Integer getValor() {
+        return valor;
+    }
+
+    public void setValor(Integer valor) {
+        this.valor = valor;
+    }
     
+ 
 }
