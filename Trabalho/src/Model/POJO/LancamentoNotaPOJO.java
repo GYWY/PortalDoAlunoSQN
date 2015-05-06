d32ef05da7ef5ae08cdce3ac5a10bf3456130eb5
wennya <wennya@wennya-Inspiron-N4050>
2015-05-06 15:28:21 -0300

    Criação de gets sets e construtores da classe LançamentoNota

--- Trabalho/src/Model/POJO/LancamentoNotaPOJO.java
+++ Trabalho/src/Model/POJO/LancamentoNotaPOJO.java
@@ -1,5 +1,18 @@
-package POJO;
+package Model.POJO;
 
 public class LancamentoNotaPOJO {
+    private Double nota;
+
+    public LancamentoNotaPOJO(Double nota) {
+        this.nota = nota;
+    }
+
+    public Double getNota() {
+        return nota;
+    }
+
+    public void setNota(Double nota) {
+        this.nota = nota;
+    }
     
 }
