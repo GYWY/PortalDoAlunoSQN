2e63576f67c65bbe311c02cab5312f89b3138e22
Guilherme <Guilherme@192.168.9.239>
2015-05-06 15:09:35 -0300

    implementação da Classe TurmaPOJO

--- Trabalho/src/Model/POJO/TurmaPOJO.java
+++ Trabalho/src/Model/POJO/TurmaPOJO.java
@@ -1,5 +1,93 @@
-package POJO;
+package Model.POJO;
+
+import java.util.Objects;
 
 public class TurmaPOJO {
     
+    private Long ano;
+    private Integer periodo;
+    private String local;
+    private String horario;
+    private Integer vaga;
+
+    public TurmaPOJO(Long ano, Integer periodo, String local, String horario, Integer vaga) {
+        this.ano = ano;
+        this.periodo = periodo;
+        this.local = local;
+        this.horario = horario;
+        this.vaga = vaga;
+    }
+
+    public Long getAno() {
+        return ano;
+    }
+
+    public void setAno(Long ano) {
+        this.ano = ano;
+    }
+
+    public Integer getPeriodo() {
+        return periodo;
+    }
+
+    public void setPeriodo(Integer periodo) {
+        this.periodo = periodo;
+    }
+
+    public String getLocal() {
+        return local;
+    }
+
+    public void setLocal(String local) {
+        this.local = local;
+    }
+
+    public String getHorario() {
+        return horario;
+    }
+
+    public void setHorario(String horario) {
+        this.horario = horario;
+    }
+
+    public Integer getVaga() {
+        return vaga;
+    }
+
+    public void setVaga(Integer vaga) {
+        this.vaga = vaga;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 7;
+        hash = 59 * hash + Objects.hashCode(this.ano);
+        hash = 59 * hash + Objects.hashCode(this.periodo);
+        hash = 59 * hash + Objects.hashCode(this.local);
+        hash = 59 * hash + Objects.hashCode(this.horario);
+        hash = 59 * hash + Objects.hashCode(this.vaga);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final TurmaPOJO other = (TurmaPOJO) obj;
+        if (!Objects.equals(this.horario, other.horario)) {
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return "TurmaPOJO{" + "ano=" + ano + ", periodo=" + periodo + ", local=" + local + ", horario=" + horario + ", vaga=" + vaga + '}';
+    }
+    
+      
 }
