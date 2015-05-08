e35b2e0e578e5db15e38dc09fabbc5f65d6520d8
wennya <wennya@wennya-Inspiron-N4050>
2015-05-08 17:48:45 -0300

    implementação da  AtividadeDAO

--- Trabalho/src/Model/DAO/AtividadeDAOImpl.java
+++ Trabalho/src/Model/DAO/AtividadeDAOImpl.java
@@ -1,34 +1,49 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package Model.DAO;
 
+import Model.POJO.AtividadePOJO;
 import java.io.Serializable;
+import java.util.ArrayList;
 import java.util.List;
 
 
 public class AtividadeDAOImpl<T, ID extends Serializable> implements GenericDAO<T, ID> {
 
+    private ArrayList<AtividadePOJO> atividades = null;
+    private static AtividadeDAOImpl instancia = null;
+
+    public AtividadeDAOImpl() {
+        atividades = new ArrayList<AtividadePOJO>();
+    }
+
+    public static synchronized AtividadeDAOImpl getInstancia() {
+        if(instancia == null){
+            instancia = new AtividadeDAOImpl();
+        }
+        
+        return instancia;
+    }
+
+    
     @Override
     public void salvar(T objeto) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+       this.atividades.add((AtividadePOJO) objeto);
     }
 
     @Override
     public void remover(T objeto) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        this.atividades.remove((AtividadePOJO) objeto);
     }
 
     @Override
     public List<T> listar() {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        for(int i = 0; i<atividades.size(); i++)
+            //printa atividades
     }
 
     @Override
     public T buscarPorId(ID id) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        
     }
     
 }
