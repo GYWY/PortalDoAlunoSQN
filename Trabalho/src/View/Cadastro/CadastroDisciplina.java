11338ef120ad491519c384f97381e6527ef0eef6
wennya <wennya@wennya-Inspiron-N4050>
2015-05-08 14:14:47 -0300

    Cadastro de disciplinas

--- Trabalho/src/View/Cadastro/CadastroDisciplina.java
+++ Trabalho/src/View/Cadastro/CadastroDisciplina.java
@@ -1,15 +1,28 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
 package View.Cadastro;
 
-/**
- *
- * @author Guilherme
- */
+import Model.DAO.DisciplinaDAOImpl;
+import Model.POJO.AlunoPOJO;
+import Model.POJO.DisciplinaPOJO;
+import java.util.Scanner;
+
+
 public class CadastroDisciplina {
     
+    public void coletarDados(DisciplinaPOJO disciplina,Scanner leitor){
+        System.out.println("Digite o nome da disciplina:   ");
+        disciplina.setNome(leitor.nextLine());
+        
+        System.out.println("Digite a ementa da disciplina:   ");
+        disciplina.setEmenta(leitor.nextLine());
+        
+        System.out.println("Digite a carga horária da matéria:  ");
+        disciplina.setCargaHoraria(leitor.nextInt());
+        
+        System.out.println("Digite o professor da discipina:  ");
+        disciplina.setProfessor(leitor.nextLine());
+        
+        System.out.println("Digite a turma:  ");
+        disciplina.setTurma(leitor.nextLine());
+    
+    }
 }
