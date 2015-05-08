6745d7ea8205819c60d3e6713dfb73d9fdf31833
Yuri Barbosa Monteiro <yurimonteiro92@hotmail.com>
2015-05-08 13:09:33 -0300

    Cadastro aluno

--- Trabalho/src/View/Cadastro/CadastroAluno.java
+++ Trabalho/src/View/Cadastro/CadastroAluno.java
@@ -6,10 +6,29 @@
 
 package View.Cadastro;
 
+import Model.DAO.AlunoDAOImpl;
+import Model.POJO.AlunoPOJO;
+import java.util.Scanner;
+
 /**
  *
  * @author Guilherme
  */
 public class CadastroAluno {
     
+    public void coletarDados(AlunoPOJO aluno,Scanner leitor){
+        System.out.println("Digite o nome do aluno:   ");
+        aluno.setNome(leitor.nextLine());
+        
+        System.out.println("Digite o C.P.F. do aluno:   ");
+        aluno.setCpf(leitor.nextLine());
+    }   
+    
+    public void cadastrarAluno(){
+        AlunoPOJO aluno = new AlunoPOJO(null,null);
+        Scanner leitor = new Scanner(System.in);
+        AlunoDAOImpl alunoDao = new AlunoDAOImpl();
+              
+        alunoDao.adicionarAluno(aluno);
+    }
 }
