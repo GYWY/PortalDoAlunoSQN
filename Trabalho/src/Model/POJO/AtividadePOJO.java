e093702c5014414be02d59fca104248820cd8d20
wennya <wennya@wennya-Inspiron-N4050>
2015-05-08 17:27:08 -0300

    mudanças de construtor

--- Trabalho/src/Model/POJO/AtividadePOJO.java
+++ Trabalho/src/Model/POJO/AtividadePOJO.java
@@ -4,18 +4,25 @@ import java.util.Objects;
 
 public class AtividadePOJO {
     //atributos
-    private String nome;//verificar com equals se repete
+    private String nome;
     private String tipo;
     private String data;
     private Float valor;
     private AlunoPOJO aluno; 
     private NotaPOJO nota;
+    private TurmaPOJO turma;
     
     //construtor
-    public AtividadePOJO(String tipo, AlunoPOJO aluno) {
-        this.tipo = tipo; 
+
+    public AtividadePOJO(String nome, String tipo, String data, Float valor, AlunoPOJO aluno, NotaPOJO nota) {
+        this.nome = nome;
+        this.tipo = tipo;
+        this.data = data;
+        this.valor = valor;
         this.aluno = aluno;
+        this.nota = nota;
     }
+   
 
     //gets e setters
     public String getNome() {
