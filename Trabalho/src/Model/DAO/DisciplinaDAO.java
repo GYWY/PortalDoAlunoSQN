8b00244c6202bab7368a07527a65decf36bdf4f6
Yuri Barbosa Monteiro <yurimonteiro92@hotmail.com>
2015-05-09 18:02:24 -0300

    Disciplina DAO da hora!!!

--- Trabalho/src/Model/DAO/DisciplinaDAO.java
+++ Trabalho/src/Model/DAO/DisciplinaDAO.java
@@ -1,19 +1,46 @@
-
 package Model.DAO;
 
+import Model.POJO.DisciplinaPOJO;
 import java.util.ArrayList;
 
-
 public class DisciplinaDAO implements GenericoDAO{
-
+    ArrayList<DisciplinaPOJO> listaDeDisciplinas = new ArrayList<>();
+    private static DisciplinaDAO instancia = null;
+    
+    public static synchronized DisciplinaDAO getInstancia() {
+        if(instancia == null){
+            instancia = new DisciplinaDAO();
+        }
+        return instancia;
+    }
+    
     @Override
     public void inserir(Object objeto) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+         DisciplinaPOJO disciplina = (DisciplinaPOJO) objeto;
+         int contador = listaDeDisciplinas.size();
+         contador++;
+         disciplina.setId(contador); //não tem id
+         listaDeDisciplinas.add(disciplina);
     }
 
     @Override
     public Object buscar(Object objeto) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        if(objeto instanceof String){
+            String nome = (String) objeto;
+            for(DisciplinaPOJO disciplina : listaDeDisciplinas){
+                if(disciplina.getNome().contains(nome))
+                    return disciplina;
+            }
+        }
+        else if(objeto instanceof Integer){
+            Integer id =(Integer) objeto;
+            for(DisciplinaPOJO disciplina : listaDeDisciplinas){
+                if(disciplina.getId().equals(objeto)){ //ver id
+                    return disciplina;
+                }
+            }
+        }
+        return null;
     }
 
     @Override
@@ -23,16 +50,22 @@ public class DisciplinaDAO implements GenericoDAO{
 
     @Override
     public boolean remover(Object objeto) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        DisciplinaPOJO disciplina = (DisciplinaPOJO) buscar(objeto);
+        if(disciplina != null){
+            listaDeDisciplinas.remove(listaDeDisciplinas.indexOf(disciplina));
+            return true;
+        }
+        return false;    
     }
 
     @Override
     public ArrayList<Object> listar(Object objeto) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        return (ArrayList<Object>)(Object)listaDeDisciplinas;
     }
 
     @Override
     public void salvarArquivo() {
+        
         throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
     }
 
