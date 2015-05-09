1aae6a4c0bb0ce1be022add7c5c938e611a784c4
Yuri Barbosa Monteiro <yurimonteiro92@hotmail.com>
2015-05-09 10:27:39 -0300

    Sobrescrevi o  equals na PessoaPOJO

--- Trabalho/src/Model/POJO/PessoaPOJO.java
+++ Trabalho/src/Model/POJO/PessoaPOJO.java
@@ -1,5 +1,7 @@
 package Model.POJO;
 
+import java.util.Objects;
+
 public class PessoaPOJO {
     private String nome;
     private String cpf;
@@ -7,6 +9,25 @@ public class PessoaPOJO {
     public PessoaPOJO(String nome,String cpf){
         this.nome = nome;
         this.cpf = cpf;
+    }
+    
+    @Override
+    public boolean equals(Object objeto){
+        if(this == objeto){
+            return true;
+        }
+        else if(!(objeto instanceof PessoaPOJO)){
+            return false;
+        }
+        else{
+            PessoaPOJO pessoa = (PessoaPOJO) objeto;
+            return this.nome.equals(pessoa.getCpf());
+        }
+    }   
+
+    @Override
+    public int hashCode() {
+        return cpf.hashCode();
     }
     
     public String getNome() {
