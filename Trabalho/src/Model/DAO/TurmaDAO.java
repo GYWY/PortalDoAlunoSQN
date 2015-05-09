42e5055e8b499cca66a8f43a9b8c50d87ee2597c
Guilherme <Guilherme@Guilherme>
2015-05-09 18:32:42 -0300

    Implementação TurmaDAO

--- Trabalho/src/Model/DAO/TurmaDAO.java
+++ Trabalho/src/Model/DAO/TurmaDAO.java
@@ -1,18 +1,40 @@
 package Model.DAO;
 
+import Model.POJO.TurmaPOJO;
 import java.util.ArrayList;
 
 
 public class TurmaDAO implements GenericoDAO{
 
+    private ArrayList<TurmaPOJO> listaDeTurmas = new ArrayList<>();
+    private static TurmaDAO instancia = null;
+    
+    public static synchronized TurmaDAO getInstancia() {
+        if(instancia == null){
+            instancia = new TurmaDAO();
+        }
+        return instancia;
+    }
+      
     @Override
     public void inserir(Object objeto) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+         TurmaPOJO turma = (TurmaPOJO) objeto;
+         int contador = listaDeTurmas.size();
+         contador++;
+         turma.setIdTurma(contador);
+         listaDeTurmas.add(turma);
     }
 
     @Override
     public Object buscar(Object objeto) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        Integer id =(Integer) objeto;
+        for(TurmaPOJO turma : listaDeTurmas){
+            if(turma.getIdTurma().equals(objeto)){
+                return turma;
+            }
+        }
+
+        return null; 
     }
 
     @Override
@@ -22,12 +44,17 @@ public class TurmaDAO implements GenericoDAO{
 
     @Override
     public boolean remover(Object objeto) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        TurmaPOJO turma = (TurmaPOJO) buscar(objeto);
+        if(turma != null){
+            listaDeTurmas.remove(listaDeTurmas.indexOf(turma));
+            return true;
+        }
+        return false;   
     }
 
     @Override
     public ArrayList<Object> listar(Object objeto) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+         return (ArrayList<Object>)(Object)listaDeTurmas;
     }
 
     @Override
