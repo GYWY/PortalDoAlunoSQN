a35e8ba349a0c532c19ebc11b619412deadf715a
Guilherme <Guilherme@172.31.24.219>
2015-05-21 14:17:15 -0300

    Implementado o método salvar  na classe AtividadeDAO

--- Trabalho/src/Model/DAO/AtividadeDAO.java
+++ Trabalho/src/Model/DAO/AtividadeDAO.java
@@ -3,9 +3,13 @@
 package Model.DAO;
 
 import Model.POJO.Atividade;
+import java.io.File;
 import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 
 public class AtividadeDAO implements GenericoDAO{
@@ -27,7 +31,11 @@ public class AtividadeDAO implements GenericoDAO{
          contador++;
          atividade.setId(contador);
          listaDeAtividades.add(atividade);
-        
+        try {
+            salvarArquivo();
+        } catch (IOException ex) {
+            Logger.getLogger(AtividadeDAO.class.getName()).log(Level.SEVERE, null, ex);
+        }
     }
 
     @Override
@@ -55,8 +63,24 @@ public class AtividadeDAO implements GenericoDAO{
         return (ArrayList<Object>)(Object)listaDeAtividades;
     }
     
-    public void salvarArquivo() {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    public void salvarArquivo() throws FileNotFoundException, IOException {
+        File arquivo;
+        arquivo = new File("C:\\Users\\Reis\\Documents\\NetBeansProjects\\PortalDoAlunoSQN\\Aluno.txt");
+        FileOutputStream fp = new FileOutputStream(arquivo);
+        String dados = "";
+        for(Atividade atividade : listaDeAtividades){
+            dados += atividade.getId()+"#"+atividade.getNome()+"#"+atividade.getData()+"#"+atividade.getTipo();//aluno.getNotaFinal().getNota()+"\n"+aluno.getFalta().getFaltas()+"\n";
+            /*dados += aluno.getListaAtividadesRealizadas().size()+"\n";
+            for(Atividade atividade : aluno.getListaAtividadesRealizadas()){
+                dados += atividade.getId()+"\n"+atividade.getTipo()+"\n"+atividade.getValor()+"\n";
+            }*/
+            //dados += aluno.getListaTurmasMatriculadas().size()+"\n";
+            /*for(Turma turma : aluno.getListaTurmasMatriculadas()){
+                dados += turma.getIdTurma()+"\n"+turma.getAno()+"\n"+turma.getHorario()+"\n"+turma.getLocal()+"\n"+turma.getPeriodo()+"\n"+turma.getProfessor()+"\n"+turma.getVaga()+"\n"+turma.getDisciplina()+"\n";
+            } */      
+        }
+        fp.write(dados.getBytes());
+        fp.close();
     }
 
     public void carregarArquivo() {
