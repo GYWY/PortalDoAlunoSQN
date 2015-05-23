09cc4d116174c6989cb08f7b14a132a41bd9cd75
Yuri Barbosa Monteiro <yurimonteiro92@hotmail.com>
2015-05-23 16:44:58 -0300

    Mudanças na discipinaView(Turma não depende de Disciplina) REVISADO!!

--- Trabalho/src/View/DisciplinaView.java
+++ Trabalho/src/View/DisciplinaView.java
@@ -2,59 +2,12 @@ package View;
 
 import Model.DAO.DisciplinaDAO;
 import Model.DAO.GenericoDAO;
-import Model.DAO.ProfessorDAO;
-import Model.DAO.TurmaDAO;
 import Model.POJO.Disciplina;
-import Model.POJO.Professor;
 import Model.POJO.Turma;
 import java.util.ArrayList;
-import java.util.Iterator;
 import java.util.Scanner;
 
 public class DisciplinaView {
-    
-    private void certificarQueOProfessorExiste(String nomeDoProfessor){
-        GenericoDAO disciplina = DisciplinaDAO.getInstancia();
-        GenericoDAO professor = ProfessorDAO.getInstancia();
-        
-        Scanner leitor = new Scanner(System.in);
-        while(professor.buscar(nomeDoProfessor) == null){
-            System.out.println("Professor não cadastrado. Digite novamente:   ");
-            nomeDoProfessor = leitor.nextLine();
-        } 
-    }
-    
-    private void cadastrarProfessorNaTurma(Turma turma){
-        Scanner leitor = new Scanner(System.in);
-        Professor professor = new Professor();
-        
-        System.out.println("Digite o nome do professor que irá ministrar a disciplina:   ");
-        this.certificarQueOProfessorExiste(leitor.nextLine());
-        turma.setProfessor(professor);
-    }
-    
-    private void lerDadosDaTurma(Turma turma){
-        Scanner leitor = new Scanner(System.in);
-        
-        System.out.println("Digite o periodo da turma:   ");
-        turma.setPeriodo(leitor.nextInt());
-        System.out.println("Digite o ano da turma:   ");
-        turma.setAno(leitor.nextLong());
-        System.out.println("Digite o local das aulas da turma:    ");
-        turma.setLocal(leitor.nextLine());
-        System.out.println("Digite o horario em que a disciplina sera ministrada:   ");
-        turma.setHorario(leitor.nextLine());
-        System.out.println("Digite a quantidade de vagas ofertadas na disciplina:   ");
-        turma.setVaga(leitor.nextInt());
-        this.cadastrarProfessorNaTurma(turma);
-    }
-    
-    public void cadastrarTurmaNaDisciplina(Disciplina disciplina){
-        Turma turma = new Turma();
-        
-        this.lerDadosDaTurma(turma);
-        disciplina.adicionarTurma(turma);
-    }
     
     public void cadastrarDisciplina() {
         Scanner leitor = new Scanner(System.in);
@@ -96,10 +49,9 @@ public class DisciplinaView {
         GenericoDAO disciplina = DisciplinaDAO.getInstancia();
         
         System.out.println("\n **************** DISCIPLINAS **************** ");
-        for (Iterator<Object> it = disciplina.listar().iterator(); it.hasNext();) {
-            Object disciplinaLista = it.next();
+        disciplina.listar().stream().forEach((disciplinaLista) -> {
             disciplinaLista.toString();
-        }
+        });
     }
 
     public void listarTurmas(String nomeDisciplina) {
@@ -107,9 +59,9 @@ public class DisciplinaView {
         
         Disciplina disciplina = (Disciplina) disciplinaDao.buscar(nomeDisciplina);
         ArrayList<Turma> listaTurma = disciplina.getListaTurmas();
-        for(Turma turma : listaTurma){
+        listaTurma.stream().forEach((turma) -> {
             turma.toString();
-        }
+        });
             
             
     }
