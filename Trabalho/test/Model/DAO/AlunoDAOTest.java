4324ca58e5cac4b4713e40344ae029a2cb2b834b
Yuri Barbosa Monteiro <yurimonteiro92@hotmail.com>
2015-05-24 18:18:15 -0300

    OS TESTES PRA ALUNO ESTÃO FUNFANDO DE BOA!!

---
+++ Trabalho/test/Model/DAO/AlunoDAOTest.java
@@ -0,0 +1,74 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Model.DAO;
+
+
+import Model.POJO.Aluno;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author linhares
+ */
+public class AlunoDAOTest {
+    
+    public AlunoDAOTest() {
+    }
+    
+    @BeforeClass
+    public static void setUpClass() {
+    }
+    
+    @AfterClass
+    public static void tearDownClass() {
+    }
+    
+    @Before
+    public void setUp() {
+    }
+    
+    @After
+    public void tearDown() {
+    }
+
+    /**
+     * Test of getInstancia method, of class AlunoDAO.
+     */
+    
+    @Test
+    public void testarBusca(){  
+        
+        AlunoDAO alunoDao = AlunoDAO.getInstancia();
+        
+        assertEquals(null,alunoDao.buscar(null));
+    }
+    
+    @Test
+    public void testarInsercao(){
+        AlunoDAO alunoDao = AlunoDAO.getInstancia();
+        Aluno aluno = new Aluno();
+        
+        aluno.setNome("Teste");
+        alunoDao.inserir(aluno);
+        Aluno alunoTeste = (Aluno) alunoDao.buscar("Teste");
+        assertEquals(alunoTeste.getNome(), aluno.getNome());
+    }
+    
+    @Test
+    //teste de arquivo vazio
+    public void testarBuscarTodos() throws IOException, FileNotFoundException, ClassNotFoundException{
+        AlunoDAO alunoDao = AlunoDAO.getInstancia();
+        
+        assertEquals(alunoDao.buscarTodos(null),false);
+    }
+}
